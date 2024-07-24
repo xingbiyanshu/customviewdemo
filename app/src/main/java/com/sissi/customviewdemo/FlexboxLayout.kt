3f62d1fac94bd8bbb822a33514b3046063dffaa3
sissi <romy_schneider@163.com>
2024-07-24 15:17:31 +0800

    add FlexboxLayout poem mode

--- app/src/main/java/com/sissi/customviewdemo/FlexboxLayout.kt
+++ app/src/main/java/com/sissi/customviewdemo/FlexboxLayout.kt
@@ -11,12 +11,30 @@ class FlexboxLayout : ViewGroup {
 
     private lateinit var context:Context
     private val childrenBounds = ArrayList<Rect>()
-    private var itemSpace=0
-    private var rowSpace=0
+    private var indent=0
+    private var itemGap=0
+    private var rowGap=0
+
+    var mode=Mode.Normal
+        set(value) {
+            field=value
+            requestLayout()
+        }
+
+    enum class Mode {
+        Normal,
+        Poem
+    }
+
     private fun init(context: Context, attrs:AttributeSet?){
         this.context = context
-        itemSpace = resources.getDimension(R.dimen.flexboxlayout_padding).toInt()
-        rowSpace = resources.getDimension(R.dimen.flexboxlayout_padding).toInt()
+        if (attrs!=null) {
+            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.FlexboxLayout)
+//            poemMode = typedArray.getBoolean(R.styleable.FlexboxLayout_poemMode, false)
+            indent = typedArray.getDimension(R.styleable.FlexboxLayout_indent, 0f).toInt()
+            itemGap = typedArray.getDimension(R.styleable.FlexboxLayout_itemGap, 0f).toInt()
+            rowGap = typedArray.getDimension(R.styleable.FlexboxLayout_rowGap, 0f).toInt()
+        }
     }
 
     constructor(context: Context) : super(context) {
@@ -32,6 +50,14 @@ class FlexboxLayout : ViewGroup {
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        if (mode==Mode.Poem){
+            measureInPoemMode(widthMeasureSpec, heightMeasureSpec)
+        }else{
+            measureInNormalMode(widthMeasureSpec, heightMeasureSpec)
+        }
+    }
+
+    private fun measureInNormalMode(widthMeasureSpec: Int, heightMeasureSpec: Int){
         //2.1 解析 ViewGroup 的父 View 对 ViewGroup 的尺寸要求
         val widthMode = MeasureSpec.getMode(widthMeasureSpec)
         val widthSize = MeasureSpec.getSize(widthMeasureSpec)
@@ -60,7 +86,7 @@ class FlexboxLayout : ViewGroup {
             if (widthMode != MeasureSpec.UNSPECIFIED && (lineWidthUsed + child.measuredWidth > rightLimit)) {
                 // 这一行的空间不够放下该child，则换行
                 lineWidthUsed = paddingLeft // 重置行偏移
-                heightUsed += (lineHeight + rowSpace) // 累加已用行高度（所有child高度以及间隙高度）
+                heightUsed += (lineHeight + rowGap) // 换行了，累加已用行高度（所有child高度以及间隙高度）
                 lineHeight = 0
                 measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, heightUsed)
             }
@@ -81,7 +107,7 @@ class FlexboxLayout : ViewGroup {
             childBound.set(lineWidthUsed, heightUsed, lineWidthUsed + child.measuredWidth, heightUsed + child.measuredHeight)
 //            childBound[lineWidthUsed, heightUsed, lineWidthUsed + child.measuredWidth] = heightUsed + child.measuredHeight
 
-            lineWidthUsed += child.measuredWidth + itemSpace
+            lineWidthUsed += child.measuredWidth + itemGap
 
             widthUsed = max(lineWidthUsed.toDouble(), widthUsed.toDouble()).toInt()
             lineHeight = max(lineHeight.toDouble(), child.measuredHeight.toDouble()).toInt()
@@ -91,6 +117,95 @@ class FlexboxLayout : ViewGroup {
         //过 setMeasuredDimension() 方法告知父 View 自己的期望尺寸
         val measuredWidth = resolveSize(widthUsed, widthMeasureSpec)
         val measuredHeight = resolveSize((heightUsed + lineHeight + paddingBottom), heightMeasureSpec)
+        setMeasuredDimension(measuredWidth, measuredHeight)
+    }
+
+    private fun measureInPoemMode(widthMeasureSpec: Int, heightMeasureSpec: Int){
+        //2.1 解析 ViewGroup 的父 View 对 ViewGroup 的尺寸要求
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        println("Mode[UNSPECIFIED=${MeasureSpec.UNSPECIFIED}, EXACTLY=${MeasureSpec.EXACTLY}, AT_MOST=${MeasureSpec.AT_MOST}]")
+        println("onMeasure: widthMode=$widthMode, widthSize=$widthSize, heightMode=$heightMode, heightSize=$heightSize")
+
+        //2.2 ViewGroup 根据「开发者在 xml 中写的对 ViewGroup 子 View 的尺寸要求」、「自己的父 View（ViewGroup 的父 View）对自己的尺寸要求」和
+        //「自己的可用空间」计算出自己对子 View 的尺寸要求，并将该尺寸要求通过子 View 的 measure() 方法传给子 View，让子 View 测量自己（View）的期望尺寸
+        var rowIntent = 0
+        var widthUsed = paddingLeft
+        var heightUsed = paddingTop
+        var lineWidthUsed = paddingLeft
+        val rightLimit = widthSize - paddingRight
+        var isForward = true
+
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            measureChildWithMargins(child, widthMeasureSpec,
+                0, // widthUsed填0以计算该child能占用多大的横向空间
+                heightMeasureSpec, heightUsed)
+
+            println("child($i/$childCount), widthMode=$widthMode, " +
+                    "lineWidthUsed=$lineWidthUsed, heightUsed=$heightUsed, child.measuredWidth=${child.measuredWidth}, " +
+                    "child.measuredHeight=${child.measuredHeight}, maxRight=$rightLimit")
+
+            // 纠错（如果item太长该行容纳不下则需要换行）
+            if (isForward){
+                if (widthMode != MeasureSpec.UNSPECIFIED && rowIntent>0 && (paddingLeft+rowIntent+child.measuredWidth > widthSize - paddingRight)) {
+                    // 这一行的空间不够放下该child，则换行
+                    rowIntent = max(rowIntent-2*indent,0) // 缩进折返
+                    measureChildWithMargins(child, widthMeasureSpec,
+                        paddingLeft+rowIntent, // 这次widthUsed我们填入真实的值，以计算child真实的横向空间大小
+                        heightMeasureSpec, heightUsed)
+                    isForward = false // 反向
+                }
+            }else{
+                // 反向intent时没有需要换行的情况
+            }
+
+            println("child($i/$childCount), widthMode=$widthMode, " +
+                    "lineWidthUsed=$lineWidthUsed, heightUsed=$heightUsed, child.measuredWidth=${child.measuredWidth}, " +
+                    "child.measuredHeight=${child.measuredHeight}, maxRight=$rightLimit")
+
+            //2.3 ViewGroup 暂时保存子 View 的尺寸，以便布局阶段和绘制阶段使用
+            var childBound: Rect
+            if (childrenBounds.size <= i) {
+                childBound = Rect()
+                childrenBounds.add(childBound)
+            } else {
+                childBound = childrenBounds[i]
+            }
+            //此处不能用 child.getxxx() 获取子 View 的尺寸值，因为子 View 只是量了尺寸，还没有布局，这些值都是 0
+//            childBound.set(child.getLeft(), child.getTop(), child.getRight(), child.getBottom());
+            childBound.set(paddingLeft+rowIntent, heightUsed, paddingLeft+rowIntent + child.measuredWidth, heightUsed + child.measuredHeight)
+//            childBound[lineWidthUsed, heightUsed, lineWidthUsed + child.measuredWidth] = heightUsed + child.measuredHeight
+
+            // 更新ViewGroup到目前为止占用的宽高
+            widthUsed = max(childBound.right, widthUsed)
+            heightUsed = childBound.bottom+rowGap
+
+            // 计算下一行的intent
+            if (isForward) {
+                rowIntent += indent
+                if (rowIntent > widthSize-paddingRight){
+                    // 折返（两种情形需要折返，一种是intent自然增长达到极限，即此处的情形；另一种是row的剩余空间存放不下item了）
+                    rowIntent = max(rowIntent-2*indent,0)
+                    isForward = false
+                }
+            }else{
+                rowIntent -= indent
+                if (rowIntent < 0){
+                    // 折返（两种情形需要折返，一种是intent自然增长达到极限，即此处的情形；另一种是row的剩余空间存放不下item了）
+                    rowIntent = indent
+                    isForward = true
+                }
+            }
+
+        }
+
+        //2.4 ViewGroup 将「根据子 View 的实际尺寸计算出的自己（ViewGroup）的尺寸」结合「自己父 View 对自己的尺寸要求」进行修正，并通
+        //过 setMeasuredDimension() 方法告知父 View 自己的期望尺寸
+        val measuredWidth = resolveSize(widthUsed+paddingRight, widthMeasureSpec)
+        val measuredHeight = resolveSize(heightUsed + paddingBottom, heightMeasureSpec)
         setMeasuredDimension(measuredWidth, measuredHeight)
     }
 
--- app/src/main/java/com/sissi/customviewdemo/FlexboxLayoutActivity.kt
+++ app/src/main/java/com/sissi/customviewdemo/FlexboxLayoutActivity.kt
@@ -1,6 +1,10 @@
 package com.sissi.customviewdemo
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,36 @@ class FlexboxLayoutActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val flexboxLayout = findViewById<FlexboxLayout>(R.id.flexbox_layout)
+        val spinner = findViewById<Spinner>(R.id.flexboxLayoutModeSpinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.flexboxLayoutMode,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+
+        spinner.setSelection(FlexboxLayout.Mode.Normal.ordinal)
+        spinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val mode = parent!!.getItemAtPosition(position) as String
+                flexboxLayout.mode = FlexboxLayout.Mode.valueOf(mode)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+        })
     }
 }
